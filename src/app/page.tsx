003042195e31f0f0f467a10aff38444a43e24b24
julian moreno <julianmoreno242@gmail.com>
2023-10-06 16:42:54 -0500

    components were imported

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
+import PatternBody from '@/components/atomic/PatternBody';
+import About from '@/components/organisms/About';
+import Feedback from '@/components/organisms/Feedback';
+import Footer from '@/components/organisms/Footer';
 import Header from '@/components/organisms/Header';
 import Hero from '@/components/organisms/Hero';
+import Patterns from '@/components/organisms/Patterns';
+import { PatternProvider } from '@/context/patternsContext';
 
 export default function Home () {
   return (
     <>
-      <Header/>
-      <main>
-        <Hero/>
-      </main>
+      <PatternProvider>
+        <Header />
+        <main className='relative'>
+          <PatternBody/>
+          <Hero />
+          <Patterns />
+          <About />
+          <Feedback/>
+        </main>
+        <Footer />
+      </PatternProvider>
     </>
   );
 }
