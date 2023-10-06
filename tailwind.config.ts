0e75b95de10ffeb5a70ad3de14f069ba114bab54
julian moreno <julianmoreno242@gmail.com>
2023-10-06 15:53:27 -0500

    removed gradient-radial

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,16 +8,6 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(rgba(173, 173, 173, 1) 0.8px, transparent 0.8px), radial-gradient(rgba(173, 173, 173, 1) 0.8px, rgba(230, 230, 247, 0) 0.8px)'
-
-      },
-      backgroundSize: {
-        'gradien-size': '12px 12px'
-      },
-      backgroundOpacity: {
-        'gradien-opacity': '0.2'
-      }
     }
   },
   plugins: [require('daisyui')]
