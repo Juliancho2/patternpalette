ea9d852bf1f6613194df92e1edc1fde119ca6dd3
julian moreno <julianmoreno242@gmail.com>
2023-10-07 15:28:40 -0500

    subtitle hero text changed

--- src/components/organisms/Hero.tsx
+++ src/components/organisms/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
 
       <div className='w-full max-w-6xl mx-auto text-center '>
         <h1 className='text-5xl p-4 text-center font-bold from-teal-500 via-teal-500 to-blue-600 bg-gradient-to-r bg-clip-text text-transparent'>Pattern Pallete</h1>
-        <p className='text-lg my-2 text-gray-400'>Take your web design to the next level</p>
+        <p className='text-lg my-2 text-gray-400'>A collection of background patterns to take your web design to the next level.</p>
         <Link href={'https://github.com/Juliancho2/patternpallete'} target='_blank'>
           <button className="btn btn-outline  btn-success text-white  mt-5 shadow-sm ">
             <Image src={github} alt='github' /> Star On Github
